3dc937d7f1454e5a28730bdf17a39e31dd04792e
vitalvirtue <hyterdem@hotmail.com>
2024-09-27 22:17:52 +0300

    feat(dump): Automatically remove the local dump file after transfer

--- src/commands/transfer.ts
+++ src/commands/transfer.ts
@@ -1,10 +1,13 @@
 import { DBArgv } from '@/common/types';
 import { getDumpCommand, getRestoreCommand } from '@/common/utils';
 import { execSync } from 'child_process';
+import fs from 'fs';
 import { green, red, yellow } from 'picocolors';
 import { ArgumentsCamelCase, Argv } from 'yargs';
 
+
 import { logger } from '../logger';
+
 
 export const command = 'transfer';
 export const describe = 'Transfers database SOURCE DB to TARGET DB, stores locally first';
@@ -19,13 +22,20 @@ export function builder(yargs: Argv<DBArgv>): Argv {
     .option('source', {
       type: 'string',
       description: 'Name of the database to count its tables from TARGET VARIABLES',
-    });
+    })
+    .option('remove', {
+      alias: 'r',
+      type: 'boolean',
+      description: 'Automatically remove the local dump file after transfer',
+      default: false,
+    });   
 }
 
 export async function handler(argv: ArgumentsCamelCase<DBArgv>) {
   try {
     const sourceDb = argv.source?.toString();
     const targetDb = argv.target?.toString();
+    const removeDump = argv.remove as boolean;
 
     logger.info(yellow(`Transfering From DB from ${sourceDb} --->>> to ${targetDb}`));
 
@@ -47,6 +57,13 @@ export async function handler(argv: ArgumentsCamelCase<DBArgv>) {
       logger.info(yellow(`Restoring file ${fileName} to DB ${targetDb}!`));
       const restoreCommand = getRestoreCommand(targetDb, fileName);
       execSync(restoreCommand);
+
+      if (removeDump) {
+        logger.info(yellow(`Removing dump file ${fileName}`));
+        fs.unlinkSync(fileName);
+        logger.info(green(`Dump file ${fileName} removed successfully`));
+      }
+
       logger.info(green(`Operation is completed`));
     } else {
       logger.error(red(` No --source and --target flag is given, can not transfer`));
